92ecde7f7ea542d2d2407fa84b002dd1337c3c42
shubham <shubhamsinghmor2312@gmail.com>
2024-08-12 00:06:27 +0530

    second changes

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express=require("express")
 const mongoose=require("mongoose");
 const Listing = require("./models/listing.js");
 const path=require("path")
+const method=require("method-override");
+const ejsMate=require("ejs-mate")
 
 let app=express();
 let port=8080;
+app.use(method("_method"));
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"))
@@ -29,6 +32,9 @@ app.get("/AirBnb",(req,res)=>{
     res.send("success")
 })
 
+app.get("/",(req,res)=>{
+    res.redirect("/listings")
+})
 //show all listings
 app.get("/listings", async (req,res)=>{
     let allListings= await Listing.find({});
@@ -45,6 +51,15 @@ app.get("/listings/new", (req,res)=>{
     res.render("./listings/new.ejs")
 })
 
+
+//create listing
+app.post("/listings",async (req,res)=>{
+    const newListing=new Listing(req.body.listing);
+   await  newListing.save()
+    res.redirect("/listings");
+})
+
+
 //show listing
 app.get("/listings/:id", async (req,res)=>{
     let {id}=req.params;
@@ -52,9 +67,22 @@ app.get("/listings/:id", async (req,res)=>{
     res.render("./listings/show.ejs",{listing})
 })
 
-//create listing
-app.post("/listings",async (req,res)=>{
-    let listing=req.body.listing;
-    console.log(listing);
+app.get("/listings/:id/edit", async (req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("./listings/edit.ejs",{listing})
 })
 
+//edit listing
+app.put("/listings/:id",async (req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`)
+})
+//delete listing
+
+app.delete("/listings/:id", async (req,res)=>{
+    let {id}=req.params;
+    let listing=await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+})
