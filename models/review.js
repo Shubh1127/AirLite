5c14012fd0a82706c132bed849c88f85c57de6a9
shubham <shubhamsinghmor2312@gmail.com>
2024-08-16 21:23:45 +0530

    changes done

---
+++ models/review.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const { type } = require("../Schema");
+const Schema=mongoose.Schema;
+
+const reviewSchema=new Schema({
+    comment:String,
+    rating:{
+        type:number,
+        min:1,
+        max:5
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    }
+})
+
+module.exports=mongoose.model("Review",reviewSchema)
