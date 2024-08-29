92ef01b269501e5fcc2effb7d5f64db0d7ee12c8
shubham <shubhamsinghmor2312@gmail.com>
2024-08-29 20:56:51 +0530

    changes done

--- public/js/map.js
+++ public/js/map.js
@@ -1,12 +1,16 @@
 mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
-    container: "map", // container ID
-    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
-    zoom: 9, // starting zoom
+  container: "map", // container ID
+  center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  zoom: 9, // starting zoom
 });
-console.log("coordinates from ejs", coordinates); // This should output something like [77.209, 28.6139]
+console.log("coordinates from ejs", listing.geometry.coordinates); // This should output something like [77.209, 28.6139]
 
 const marker = new mapboxgl.Marker({ color: "red" })
-  .setLngLat(coordinates)
-  .addTo(map);
+  .setLngLat(listing.geometry.coordinates)
+  .addTo(map)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(`<h4>${listing.location}
+    </h4><p>Exact Location provided after the booking</p>`)
+  );
